40e9e4f65cbf0db16b9b7349a43d8116438b7be2
Usman <usman.mughal@outlook.fr>
2023-11-15 12:40:53 +0100

    intégration du header

--- sportsee/src/Components/Header.jsx
+++ sportsee/src/Components/Header.jsx
@@ -1,8 +1,30 @@
+import { NavLink } from "react-router-dom";
+import logo from '@/Icones/logo.svg';
+
 function Header () {
   return (
-    <div> Header
-    </div>
+    <header className="header-main">
+      <NavLink to="/">
+        <img className="logo" src={logo} alt="logo de SportSee" />
+      </NavLink>
+      <nav className="navbar">
+        <ul className="navbarList">
+          <li className="navbarListItem">
+            <NavLink className="navbarLink" to="/">Accueil</NavLink>
+          </li>
+          <li className="navbarListItem">
+            <NavLink className="navbarLink" to="/profil">Profil</NavLink>
+          </li>
+          <li className="navbarListItem">
+            <NavLink className="navbarLink" to="/reglages">Réglages</NavLink>
+          </li>
+          <li className="navbarListItem">
+            <NavLink className="navbarLink" to="/communaute">Communauté</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
-
+  
 export default Header;
